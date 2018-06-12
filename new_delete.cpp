50a4129b4745939bba630a41b0b4f40df2dae08d
vjain85 <vikas.jain8596@gmail.com>
2018-06-12 17:12:42 +0530

    added comments to new_delete

--- new_delete.cpp
+++ new_delete.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
 using namespace std;
+// dynamic memory allocation in c++
 
 int main()
 {
     int n,*p;
     cout << "\n Input no. of vars: ";
     cin >> n;
-    p = new int[n];
+    p = new int[n]; // p = (int)malloc(n*sizeof(int));
     for(int x = 0; x < n ; x++)
     {
         cout << "\nInput: ";
@@ -16,7 +17,7 @@ int main()
     {
         cout<< "\n "<<p[x];
     }
-    delete []p;
+    delete []p; // free(p);
     return 0;
 }
 
